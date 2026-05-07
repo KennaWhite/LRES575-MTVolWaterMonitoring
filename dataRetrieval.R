fd66f111b6f4ba73f1a8192813dc1eb9d65d2b0a
KennaWhite <77936317+KennaWhite@users.noreply.github.com>
2026-05-07 09:38:55 -0600

    Update and rename physChem.R to dataRetrieval.R

--- dataRetrieval.R
+++ dataRetrieval.R
@@ -1,16 +1,20 @@
 # Setup -----------------------------------------------------------------------------------------------------------
 cat("\014") # clear console
 rm(list=ls()); # remove all objects from workspace
-setwd("~/Desktop/Masters/ProfessionalPaper/Rdata")
+setwd("~/Desktop")
 library(dataRetrieval)
 library(dplyr)
 library(readr)
 library(purrr)
-library(lubridate)
 library(tidyverse)
 library(treemapify)
-
-#### IF STARTING A NEW PROJECT READ IN THESE FILES ------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+### IF STARTING A NEW PROJECT IN R READ IN THESE FILES FIRST, IF NOT CONTINUE TO STEP 1
+--------------------------------------------------------------------------------------------------------------------
+#ProjectIDs.csv is located in the inputs folder and lists all the volunteer groups' simple IDs
+#All_WQP_PhysChemData_Cleaned.csv includes all the cleaned physical and chemical data from the WQX portal that will be the final output of this script
+#Summary_PhysChemData_Cleaned.csv is this script's output summarized into a single dataframe by project ID
+--------------------------------------------------------------------------------------------------------------------
 ProjectIDs <- read.csv('ProjectIDs.csv', 
                        colClasses = c(ProjectID_Simple = "factor", ProjectIdentifier = "character",Rank="factor"))%>%
   filter(Rank=="1"|Rank=="2")
@@ -22,19 +26,18 @@ all_wqp_PhysChemData<-all_wqp_PhysChemData %>%
   mutate(OrganizationFormalName= as.character(OrganizationFormalName),
          ResultMeasure.MeasureUnitCode=as.factor(ResultMeasure.MeasureUnitCode),
          DetectionQuantitationLimitMeasure.MeasureValue=as.numeric(DetectionQuantitationLimitMeasure.MeasureValue),
-         DetectionQuantitationLimitMeasure.MeasureUnitCode=as.factor(DetectionQuantitationLimitMeasure.MeasureUnitCode),
+        DetectionQuantitationLimitMeasure.MeasureUnitCode=as.factor(DetectionQuantitationLimitMeasure.MeasureUnitCode),
          ActivityStartDate=as.Date(ActivityStartDate),
          ResultMeasureValue=as.numeric(ResultMeasureValue)) 
 
 summary_df<- read.csv('Summary_PhysChemData_Cleaned.csv')
-
-###Create a file to relate the project IDs to groups ---------------------------------------------------------------
-
+--------------------------------------------------------------------------------------------------------------------
+### 1. Create a file to relate the project IDs to groups 
 ProjectIDs <- read.csv('ProjectIDs.csv', 
                        colClasses = c(ProjectID_Simple = "factor", ProjectIdentifier = "character",Rank="factor"))%>%
   filter(Rank=="1"|Rank=="2")
-
-###Create a function to download narrow data by Project ID
+--------------------------------------------------------------------------------------------------------------------
+### 2. Create a function to download narrow physical & chemical data by Project ID
 get_project_data <- function(proj_id) {
   tryCatch({
     data <- readWQPdata(project = proj_id, dataProfile = "resultPhysChem")
@@ -48,14 +51,13 @@ get_project_data <- function(proj_id) {
     error = function(e) {
       message("Error retrieving data for ", proj_id, ": ", e$message)
       return(NULL)})}
-
-### Download the phys results for all the groups and then join into one dataframe -------------
+--------------------------------------------------------------------------------------------------------------------
+### 3. Download the phys results for all the groups and then join into one dataframe 
 
 all_wqp_PhysChemData <- map_dfr(ProjectIDs$ProjectIdentifier, get_project_data)
 all_wqp_PhysChemData <- left_join(all_wqp_PhysChemData, ProjectIDs, by = "ProjectIdentifier")
 
-### Keep only selected columns ----------------------------------------------------------------
-
+      # Keep only selected columns from the file
 cols_to_keep <- c(
   "ProjectIdentifier",
   "ProjectID_Simple",
@@ -72,12 +74,11 @@ cols_to_keep <- c(
   "ActivityLocation.LatitudeMeasure",
   "ActivityLocation.LongitudeMeasure",
   "MonitoringLocationName")
-
-###Filter to only the necessary columns---------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+### 4. Filter to only the necessary columns and clean the data types in each column
 
 all_wqp_PhysChemData <- all_wqp_PhysChemData[, intersect(cols_to_keep, names(all_wqp_PhysChemData))]
 
-### Clean the data types in each column---------------------------------------------------------------------
 all_wqp_PhysChemData<-all_wqp_PhysChemData %>%
   mutate(OrganizationFormalName= as.character(OrganizationFormalName),
          ResultMeasure.MeasureUnitCode=as.factor(ResultMeasure.MeasureUnitCode),
@@ -85,17 +86,18 @@ all_wqp_PhysChemData<-all_wqp_PhysChemData %>%
          DetectionQuantitationLimitMeasure.MeasureUnitCode=as.factor(DetectionQuantitationLimitMeasure.MeasureUnitCode),
          ActivityStartDate=as.Date(ActivityStartDate),
          CharacteristicName=as.factor(CharacteristicName))
-
-### Make the NA values in results 1/2 the detection limit --------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+### 5. Make the NA values in results 1/2 the detection limit so they do not throw NA errors later in the script
 
 all_wqp_PhysChemData$ResultMeasureValue[is.na(all_wqp_PhysChemData$ResultMeasureValue)] <- all_wqp_PhysChemData$DetectionQuantitationLimitMeasure.MeasureValue[is.na(all_wqp_PhysChemData$ResultMeasureValue)] / 2
 all_wqp_PhysChemData$ResultMeasure.MeasureUnitCode[is.na(all_wqp_PhysChemData$ResultMeasure.MeasureUnitCode)] <- all_wqp_PhysChemData$DetectionQuantitationLimitMeasure.MeasureUnitCode[is.na(all_wqp_PhysChemData$ResultMeasure.MeasureUnitCode)] 
+--------------------------------------------------------------------------------------------------------------------
+### 6. Export the final script and its summary script 
 
 write_csv(all_wqp_PhysChemData,'All_WQP_PhysChemData_Cleaned.csv')
-
 summary(all_wqp_PhysChemData)
 
-### Summarize and group the data by ProjectID_Simple -----------------------------------------------------
+    #Summarize and group the data by ProjectID_Simple 
 
 summary_df <- all_wqp_PhysChemData %>%
   group_by(ProjectID_Simple) %>%
@@ -107,32 +109,7 @@ summary_df <- all_wqp_PhysChemData %>%
     n_SamplingEvents = n_distinct(ActivityIdentifier),
     n_SamplingDates = n_distinct(ActivityStartDate),
     Parameter_List = str_c(sort(unique(CharacteristicName)), collapse = ","),
-    .groups = "drop"
-  ) %>%
+    .groups = "drop") %>%
   arrange(ProjectID_Simple)
 
 write_csv(summary_df,'Summary_PhysChemData_Cleaned.csv')
-
-### Create a lat and long dataframe and remove duplicate rows-------------------------------------------------------
-
-project_locations <- all_wqp_PhysChemData %>%
-  select(ProjectID_Simple,
-         MonitoringLocationName,
-         ActivityLocation.LatitudeMeasure,
-         ActivityLocation.LongitudeMeasure) %>%
-         distinct() %>%   
-  arrange(ProjectID_Simple, MonitoringLocationName)
-
-projectDate_locations <- all_wqp_PhysChemData %>%
-  select(ProjectID_Simple,
-         MonitoringLocationName,
-         ActivityLocation.LatitudeMeasure,
-         ActivityLocation.LongitudeMeasure,ActivityStartDate) %>%
-  distinct() %>%   
-  arrange(ProjectID_Simple, MonitoringLocationName)
-
-write_csv(project_locations,'project_locations_lat_long.csv')
-write_csv(projectDate_locations,'projectDate_locations_lat_long.csv')
-
-
-
